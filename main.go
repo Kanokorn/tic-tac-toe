edc121e3c6f1d530a621e5002ae1680504ea87b6
Kanokorn <kanokorn.may@gmail.com>
2016-09-17 11:13:15 +0700

    make program receive position of 'X' and 'O'

--- main.go
+++ main.go
@@ -9,10 +9,31 @@ func main() {
 	var input string
 	var n int
 	position := [9]rune{'1', '2', '3', '4', '5', '6', '7', '8', '9'}
+	player := 1
+	value := 'X'
 
 	fmt.Println("Tic Tac Toe")
 	fmt.Println("Player 1 (X) - Player 2 (O)")
 
+	for {
+		displayTable(position)
+
+		fmt.Printf("Player %d, enter a number: ", player)
+		fmt.Scanln(&input)
+		n, _ = strconv.Atoi(input)
+		position[n-1] = value
+
+		if player == 1 {
+			player = 2
+			value = 'O'
+		} else {
+			player = 1
+			value = 'X'
+		}
+	}
+}
+
+func displayTable(position [9]rune) {
 	fmt.Println("   :   :   ")
 	fmt.Printf(" %c : %c : %c \n", position[0], position[1], position[2])
 	fmt.Println("___:___:___")
@@ -22,20 +43,4 @@ func main() {
 	fmt.Println("   :   :   ")
 	fmt.Printf(" %c : %c : %c \n", position[6], position[7], position[8])
 	fmt.Println("   :   :   ")
-
-	fmt.Print("Player 1, enter a number: ")
-	fmt.Scanln(&input)
-	n, _ = strconv.Atoi(input)
-	position[n-1] = 'X'
-
-	fmt.Println("   :   :   ")
-	fmt.Printf(" %c : %c : %c \n", position[0], position[1], position[2])
-	fmt.Println("___:___:___")
-	fmt.Println("   :   :   ")
-	fmt.Printf(" %c : %c : %c \n", position[3], position[4], position[5])
-	fmt.Println("___:___:___")
-	fmt.Println("   :   :   ")
-	fmt.Printf(" %c : %c : %c \n", position[6], position[7], position[8])
-	fmt.Println("   :   :   ")
-
 }
